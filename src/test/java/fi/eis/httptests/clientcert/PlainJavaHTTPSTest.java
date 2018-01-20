1425dfae10046a877be4fd9c888f2996aba5ecbf
eis <eis@iki.fi>
2018-01-20 13:22:40 +0200

    Fixed getting absolute path of a file

--- src/test/java/fi/eis/httptests/clientcert/PlainJavaHTTPSTest.java
+++ src/test/java/fi/eis/httptests/clientcert/PlainJavaHTTPSTest.java
@@ -9,12 +9,14 @@ import javax.net.ssl.HttpsURLConnection;
 public class PlainJavaHTTPSTest {
 
     @BeforeClass
-    public static void setUp() {
-        System.setProperty("javax.net.ssl.keyStore",
-                new File(".").getAbsolutePath() + "/src/test/resources/fi/eis/httptests/clientkeystore-samepass.jks");
+    public static void setUp() throws Exception {
+        System.setProperty("javax.net.ssl.keyStore",getAbsPath("clientkeystore-samepass.jks"));
         System.setProperty("javax.net.ssl.keyStorePassword", "keystorepass");
     }
 
+    static String getAbsPath(String fileName) throws Exception {
+        return new File(PlainJavaHTTPSTest.class.getResource(fileName).toURI()).getAbsolutePath();
+    }
     @Test
     public void testPlainJavaHTTPS() throws Exception {
         String httpsURL = "https://slsh.iki.fi/client-certificate/protected/";
